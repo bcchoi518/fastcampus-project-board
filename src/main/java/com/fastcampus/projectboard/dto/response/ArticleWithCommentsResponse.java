cc210bfcf1d0d806febb3f17f4c3b8776aabe0cc
bcchoi518 <bcchoi518@gmail.com>
2022-11-12 13:16:27 +0900

    #21 - 게시판 컨트롤러 구현
    게시판 기본 조회 화면을 위한 컨트롤러 구현하고 
    데이터 전달 완료
    게시판 페이지 기본 정렬 속성 부여: 10개 1페이지, 시간 내림차순

--- src/main/java/com/fastcampus/projectboard/dto/response/ArticleWithCommentsResponse.java
+++ src/main/java/com/fastcampus/projectboard/dto/response/ArticleWithCommentsResponse.java
@@ -7,7 +7,7 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public record ArticleWithCommentResponse(
+public record ArticleWithCommentsResponse(
         Long id,
         String title,
         String content,
@@ -15,19 +15,19 @@ public record ArticleWithCommentResponse(
         LocalDateTime createdAt,
         String email,
         String nickname,
-        Set<ArticleCommentResponse> articleCommentResponses
+        Set<ArticleCommentResponse> articleCommentsResponse
 ) {
-    public static ArticleWithCommentResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, Set<ArticleCommentResponse> articleCommentResponses) {
-        return new ArticleWithCommentResponse(id, title, content, hashtag, createdAt, email, nickname, articleCommentResponses);
+    public static ArticleWithCommentsResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, Set<ArticleCommentResponse> articleCommentResponses) {
+        return new ArticleWithCommentsResponse(id, title, content, hashtag, createdAt, email, nickname, articleCommentResponses);
     }
 
-    public static ArticleWithCommentResponse from(ArticleWithCommentsDto dto) {
+    public static ArticleWithCommentsResponse from(ArticleWithCommentsDto dto) {
         String nickname = dto.userAccountdto().nickname();
         if (nickname == null || nickname.isBlank()) {
             nickname = dto.userAccountdto().userId();
         }
 
-        return new ArticleWithCommentResponse(
+        return new ArticleWithCommentsResponse(
                 dto.id(),
                 dto.title(),
                 dto.content(),
